e50b49ae78461f44554cb5e5ec2e372d8a3c8391
MohaElder <calen0909@hotmail.com>
2022-09-22 22:06:08 -0700

    added a servlet version of the demo

---
+++ ServletAttempt/ServletWrapper.java
@@ -0,0 +1,37 @@
+
+// Import required java libraries
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Extend HttpServlet class
+public class ServletWrapper extends HttpServlet {
+
+   private ProgramClass program;
+
+   public void init() throws ServletException {
+      // Do required initialization
+      program = new ProgramClass();
+   }
+
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+
+      String ret = "";
+
+      if (request.getQueryString() == null) {
+         ret = program.handleRequest(
+               request.getRequestURI());
+      } else {
+         ret = program.handleRequest(
+               request.getRequestURI() + "?" + request.getQueryString());
+      }
+      // Actual logic goes here.
+      PrintWriter out = response.getWriter();
+      out.println("<h1>" + ret + "</h1>");
+   }
+
+   public void destroy() {
+      // do nothing.
+   }
+}
