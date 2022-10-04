58a5173699a7d457a8d4c9c5b9e9a13e9a9c4be9
Joe Politz <joe@cs.brown.edu>
2022-10-04 10:45:42 -0700

    .

--- NumberServer.java
+++ NumberServer.java
@@ -27,7 +27,7 @@ class Handler implements URLHandler {
     }
 }
 
-class Main {
+class NumberServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
