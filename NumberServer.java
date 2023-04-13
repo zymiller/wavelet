344fc69155f048fc29f7777b267dae53acb05668
zymiller <zymiller@ucsd.edu>
2023-04-12 17:38:59 -0700

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -8,7 +8,7 @@ class Handler implements URLHandler {
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
+            return String.format("Zach's Number: %d", num);
         } else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
